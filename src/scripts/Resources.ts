c40a9c814ace39f02182e28718f3032361178e35
danielcollishaw <cubita98@me.com>
2023-12-11 23:39:48 -0500

    adding future variables

--- src/scripts/Resources.ts
+++ src/scripts/Resources.ts
@@ -5,13 +5,19 @@ interface ResourceStore {
   toggleVisible: () => void;
 
   barks: number;
+  dogs: number;
+  barkRate: () => number;
+  addToDogs: (n: number) => void;
   addToBark: (n: number) => void;
 }
 
-export const useResourceStore = create<ResourceStore>((set) => ({
+export const useResourceStore = create<ResourceStore>((set, get) => ({
   visible: false,
   toggleVisible: () => set((state: any) => ({ visible: !state.visible })),
 
   barks: 0,
-  addToBark: (n: number) => set((state: any) => ({ barks: state.barks + n })),
+  dogs: 0,
+  barkRate: () => get().dogs * 1.05,
+  addToDogs: (n) => set((state: any) => ({ barks: state.dogs + n })),
+  addToBark: (n) => set((state: any) => ({ barks: state.barks + n })),
 }));
